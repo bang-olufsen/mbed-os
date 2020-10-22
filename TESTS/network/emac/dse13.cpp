b368898144c925b7b2d9ecb7ad305fe8e2a15b9f
Martin Hans <extmaha@bang-olufsen.dk>
2020-10-22 09:45:44 +0200

    B&O: Choose non-colliding MAC addresses during EMAC test
    The EMAC test requires two devices on the same ethernet segment. This
    change makes sure that the two devices choose different MAC addresses.

--- TESTS/network/emac/dse13.cpp
+++ TESTS/network/emac/dse13.cpp
@@ -12,3 +12,20 @@ void init_dse13() {
 
     osDelay(5000); // wait for the Firefox to enable the switch
 }
+
+
+//! @brief   Defines the platform's MAC address
+//! This function is called by mbedOS to retrieve the MAC that the interface should use.
+//! @param   mac A 6-byte array defining the MAC address
+extern "C" void mbed_mac_address(char* mac)
+{
+    // We don't have access to a random source or to a unique MAC address, but by putting the platforms and the role of the executable 
+    // in the test setup we can choose MAC addresses that are unlikely to collide in the test setup.
+    unsigned char macAddress[6] = { 0x00, 0x09, 0xa7, 0x08, 0x00, 0x00 };
+    macAddress[4] = MBED_CONF_APP_ECHO_SERVER;
+    #if defined(BOARD_EVAL)
+    macAddress[5] = 1;
+    #endif
+
+    memcpy(mac, macAddress, sizeof(macAddress));
+};
