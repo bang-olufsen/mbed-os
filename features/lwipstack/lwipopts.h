b563c9ab93e2d9faf1868501f25f62a8115019bd
Martin Hans <extmaha@bang-olufsen.dk>
2020-12-03 10:10:16 +0100

    DSEM-164 I want configure MDNS using the standard lwIP interface, as a developer  (#1)
    * B&O Enable MDNS responder
    
    * B&O: Expose getters to give access to the lwIP netif instance
    
    * FIXME: Do not merge: Enable debug output
    
    * Revert "FIXME: Do not merge: Enable debug output"
    
    This reverts commit 2137384814899f82e2e0c86f93b0ec9c3096d0e8.
    
    * Revert "B&O: Expose getters to give access to the lwIP netif instance"
    
    This reverts commit 1279b74dc24c9c456ee8e639a74d7756af3a6068.
    
    Co-authored-by: Antoine Wajntraub <anwa@bang-olufsen.dk>

--- features/lwipstack/lwipopts.h
+++ features/lwipstack/lwipopts.h
@@ -276,6 +276,8 @@
 #define LWIP_FORCE_AUTOIP 1
 #define LWIP_AUTOIP_RAND(netif) LWIP_RAND()
 #define LWIP_AUTOIP_CREATE_SEED_ADDR(netif) lwip_htonl(AUTOIP_RANGE_START + (LWIP_RAND() & 0xffff))
+#define LWIP_MDNS_RESPONDER 1
+#define LWIP_NUM_NETIF_CLIENT_DATA 1
 
 #if MBED_CONF_LWIP_ETHERNET_ENABLED
 #define LWIP_ARP                    1
